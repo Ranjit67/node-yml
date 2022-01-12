473fc178523d0eed7ceaee2b14e455ebbc369673
Ranjit67 <ranjitsahoo873@gmail.com>
2022-01-12 12:25:19 +0530

    update language

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -1,4 +1,18 @@
 import { Router } from "express";
 import { ServiceController } from "../controllers";
 
-class ServiceRoute {}
+class ServiceRoute {
+  public router = Router();
+  public path = "/service";
+  private serviceController = new ServiceController();
+  constructor() {
+    this.routes();
+  }
+  private routes() {
+    this.router.post("/create", this.serviceController.create);
+    this.router.get("/all-services", this.serviceController.getAll);
+    this.router.get("/all-services/:id", this.serviceController.getOne);
+    this.router.put("/update", this.serviceController.update);
+    this.router.delete("/delete", this.serviceController.delete);
+  }
+}
