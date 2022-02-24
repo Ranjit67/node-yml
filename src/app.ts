66d1651b19cedb8b893466bbacb977e09c8113e4
Ranjit67 <ranjitsahoo873@gmail.com>
2022-02-24 18:29:09 +0530

    packege remove

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,9 @@
 import express, { Application } from "express";
 import { port, connectionDB } from "./config";
 import fileUpload from "express-fileupload";
-import swaggerDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
+
 import log from "./logger";
 
-// import schedule from "node-schedule";
-// const schedule = require("node-schedule");
-
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 class App {
   public app: Application;
   constructor() {
@@ -17,40 +12,6 @@ class App {
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(fileUpload());
     // scheduler
-
-    //
-    const option = {
-      swaggerDefinition: {
-        openapi: "3.0.0",
-        info: {
-          title: "API",
-          version: "1.0.0",
-        },
-        servers: [
-          {
-            url: "http://localhost:4000",
-          },
-        ],
-        components: {
-          securitySchemes: {
-            bearerAuth: {
-              type: "http",
-              scheme: "bearer",
-              bearerFormat: "JWT",
-            },
-          },
-        },
-        security: [
-          {
-            bearerAuth: [],
-          },
-        ],
-      },
-      apis: ["./src/routes/*.routes.ts"],
-    };
-    const swaggerSpace = swaggerDoc(option);
-    this.app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpace));
-    //
   }
   public listen(appInt: {
     controllers: any[];
