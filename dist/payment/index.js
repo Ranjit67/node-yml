1d4da000db31cf835558c6bbef83d6f6a1a4a868
Ranjit67 <ranjitsahoo873@gmail.com>
2022-02-27 13:22:33 +0530

    chek the path

--- dist/payment/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PaymentRoutes = void 0;
-var routes_1 = require("./routes");
-Object.defineProperty(exports, "PaymentRoutes", { enumerable: true, get: function () { return routes_1.PaymentRoutes; } });
-//# sourceMappingURL=index.js.map
--- src/middleware/bottom.middleware.ts
+++ src/middleware/bottom.middleware.ts
@@ -14,7 +14,7 @@ class BottomMiddleware {
     res: Response,
     next: NextFunction
   ) {
-    next(new NotFound("No route found, Please check your url."));
+    next(new NotFound("No route found, Please check your url2."));
   }
   public fromRouteErrorHandler(
     err: any,
